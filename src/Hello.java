28e73914f8581d359b778293651f0a86535042d5
Shin-moohwan <62367684+Shin-moohwan@users.noreply.github.com>
2020-03-19 23:08:44 +0900

    create a menu

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello{
+
+	public static void main(String[] args) {
+		System.out.println("*** Id Management System Menu ***");
+		System.out.println("1. Add Id");
+		System.out.println("2. Delete Id");
+		System.out.println("3. Edit Id");
+		System.out.println("4. View Id");
+		System.out.println("5. Exit");
+		System.out.println("Select one number between 1 -5");
+	}
+}
