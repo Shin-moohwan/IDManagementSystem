524cc237d7b5b5184f3e7331db970b7afccf19ba
Shin-moohwan <62367684+Shin-moohwan@users.noreply.github.com>
2020-03-21 17:25:52 +0900

    some modifications

--- src/IdManager.java
+++ src/IdManager.java
@@ -1,11 +1,10 @@
 public class IdManager{
 	public static void main(String[] args) {
-		System.out.println("*** Id Management System Menu ***");
+		System.out.println("### Id Management System Menu ###");
 		System.out.println("1. Add Id");
 		System.out.println("2. Delete Id");
 		System.out.println("3. Edit Id");
 		System.out.println("4. View Id");
 		System.out.println("5. Exit");
-		System.out.println("Select one number between 1 -5");
 	}
 }
