9039313a80f1408bc4c77d432ab1e70cff08cfbe
Shin-moohwan <62367684+Shin-moohwan@users.noreply.github.com>
2020-04-05 23:34:11 +0900

    modified the edit menu

--- src/IdManager.java
+++ src/IdManager.java
@@ -34,7 +34,36 @@ public class IdManager {
 		System.out.print("Your Id number:");
 		int idnumber = input.nextInt();
 		if (id.idnumber == idnumber) {
-			System.out.println("the id to be edited is " + idnumber);
+			int num = -1;
+			while (num != 5) {
+				System.out.println("## Id Info Edit Menu ##");
+				System.out.println("1. Edit Id");
+				System.out.println("2. Edit Idsite");
+				System.out.println("3. Edit Password");
+				System.out.println("4. Edit Id number");
+				System.out.println("5. Exit");
+				System.out.println("Select one number between 1 - 5:");
+				num = input.nextInt();
+				if (num == 1) {
+					System.out.print("Your Id:");
+					id.yourid = input.next();
+				}
+				else if (num == 2) {
+					System.out.print("Id Site:");
+					id.idsite = input.next();
+				}
+				else if (num == 3) {
+					System.out.print("Id password:");
+					id.password = input.next();
+				}
+				else if (num == 4) {
+					System.out.print("Id number:");
+					id.idnumber = input.nextInt();
+				}
+				else {
+					continue;
+				}
+			}
 		}
 	}
 	public void viewId() {
